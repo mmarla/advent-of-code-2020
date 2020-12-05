6dd95cd6c45d24ca0490f91afdde4872340d7282
Susana Rodriguez <susana.rodriguez@bbc.co.uk>
2020-12-05 18:14:59 +0000

    Refactor Day 5

--- day5/index.js
+++ day5/index.js
@@ -1,76 +1,60 @@
 const getMid = (min, max) => (max - min) / 2;
 
-const getUpper = ({ min, max }) => {
-  const newLower = min + getMid(min, max);
-  return {
-    min: newLower,
-    max,
-  };
-};
+const selectUpper = ({ min, max }) => ({
+  min: min + getMid(min, max),
+  max,
+});
 
-const getLower = ({ min, max }) => {
-  const newUpper = max - getMid(min, max);
-  return {
-    min,
-    max: newUpper,
-  };
-};
+const selectLower = ({ min, max }) => ({
+  min,
+  max: max - getMid(min, max),
+});
 
 const chooseHighest = (range) => range.max - 1;
+
 const chooseLowest = (range) => range.min;
 
 const mapped = {
-  F: getLower,
-  L: getLower,
-  B: getUpper,
-  R: getUpper,
+  F: selectLower,
+  L: selectLower,
+  B: selectUpper,
+  R: selectUpper,
 };
 
-const otherMap = {
+const last = {
   F: chooseLowest,
   L: chooseLowest,
   B: chooseHighest,
   R: chooseHighest,
 };
 
-const getList = (data) => {
-  const lala = data.map((line) => {
-    const values = line.split("").reduce(
-      (range, letter, index) => {
-        if (index === 9) {
-          return { ...range, column: otherMap[letter](range) };
-        }
-        if (index === 6) {
-          return { min: 0, max: 8, row: otherMap[letter](range) };
-        }
-        if (index > 6) {
-          const newRange = mapped[letter](range);
-          return { ...range, ...newRange };
-        }
-        const newRange = mapped[letter](range);
+const getPosition = (arr, max) =>
+  arr.reduce(
+    (range, letter, i) => {
+      if (i === arr.length - 1) return last[letter](range);
 
-        return { ...range, ...newRange };
-      },
-      { min: 0, max: 128 }
-    );
-    return values.row * 8 + values.column;
-  });
-  return lala.sort((a, b) => a - b);
-};
+      return { ...range, ...mapped[letter](range) };
+    },
+    { min: 0, max }
+  );
 
-const getHighestBoardingPass = (data) =>
+const getList = (data) =>
+  data
+    .map((line) => {
+      const row = getPosition(line.substring(0, 7).split(""), 128);
+      const column = getPosition(line.substring(7).split(""), 8);
+
+      return row * 8 + column;
+    })
+    .sort((a, b) => a - b);
+
+const getHighest = (data) =>
   getList(data).filter((_, i, arr) => i === arr.length - 1)[0];
 
-const getMissingBoardingPass = (data) => {
-  const numArray = getList(data);
-  let number = 0;
+const getMissing = (data) =>
+  getList(data).reduce(
+    (acc, num, i, arr) => (num - arr[i - 1] !== 1 ? num - 1 : acc),
+    0
+  );
 
-  for (var i = 0; i < numArray.length; i++) {
-    if (numArray[i] - numArray[i - 1] != 1) {
-      number = numArray[i - 1] + 1;
-    }
-  }
-  return number;
-};
-
-module.exports = { getHighestBoardingPass, getMissingBoardingPass };
+module.exports = { getHighest, getMissing };
--- day5/index.test.js
+++ day5/index.test.js
@@ -1,4 +1,4 @@
-const { getHighestBoardingPass, getMissingBoardingPass } = require("./index");
+const { getHighest, getMissing } = require("./index");
 const readFile = require("../helpers");
 const simpleInput = "./day5/simple-input.txt";
 const complexInput = "./day5/complex-input.txt";
@@ -6,17 +6,17 @@ const complexInput = "./day5/complex-input.txt";
 test("finds boarding passes - simple input", async () => {
   const data = await readFile(simpleInput);
 
-  expect(getHighestBoardingPass(data)).toBe(820);
+  expect(getHighest(data)).toBe(820);
 });
 
 test("finds boarding passes - simple input", async () => {
   const data = await readFile(complexInput);
 
-  expect(getHighestBoardingPass(data)).toBe(947);
+  expect(getHighest(data)).toBe(947);
 });
 
 test("finds missing boarding pass - complex input", async () => {
   const data = await readFile(complexInput);
 
-  expect(getMissingBoardingPass(data)).toBe(636);
+  expect(getMissing(data)).toBe(636);
 });
